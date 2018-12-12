a9a6296a26f6510c2c4778787262e4ef5be32729
Alex Tareshawty <atareshawty@gmail.com>
2018-12-12 11:45:00 -0500

    Adds spec to assert all cops are required in config

---
+++ spec/config/rubocop_config_spec.rb
@@ -0,0 +1,17 @@
+require_relative "../spec_helper.rb"
+require "yaml"
+
+RSpec.describe ".rubocop.yml" do
+  it "includes all of our cops", :aggregate_failures do
+    config_location = File.expand_path("../../.rubocop.yml", File.dirname(__FILE__))
+    rubocop_config = YAML.load_file(config_location)
+    required_libs = rubocop_config["require"].map { |lib| lib.sub("\./", "") }
+
+    cops_location = File.expand_path("../../lib/rubocop/cop/root_cops", File.dirname(__FILE__))
+    Dir.glob("#{cops_location}/**/*") do |cop_file|
+      next unless File.file?(cop_file)
+      cop_file_required = required_libs.any? { |lib| cop_file.end_with?(lib) }
+      expect(cop_file_required).to be(true), "Expected #{cop_file} to be included in project's .rubocop.yml config"
+    end
+  end
+end
