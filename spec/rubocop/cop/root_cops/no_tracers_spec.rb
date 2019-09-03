1bff701b45788575e36b4945e49811e511897f04
Keith Wedinger <keith.wedinger@joinroot.com>
2019-09-03 15:39:03 -0400

    Add NoTracers check

---
+++ spec/rubocop/cop/root_cops/no_tracers_spec.rb
@@ -0,0 +1,13 @@
+require_relative "../../../spec_helper.rb"
+
+describe RootCops::NoTracers do
+  OFFENDING_FORMS = [
+    "Tracer.trace_method".freeze
+  ].freeze
+
+  subject(:cop) { described_class.new }
+
+  OFFENDING_FORMS.each do |offending_form|
+    it_behaves_like "registers an offense", offending_form, described_class::MSG
+  end
+end
