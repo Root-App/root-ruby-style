42b78779f31b7efb99e7cf122945992043fe1a8f
Camille Neuner <camille.neuner@joinroot.com>
2020-02-10 16:35:11 -0600

    This cop should consider the new sharded_queue_as option

--- lib/rubocop/cop/root_cops/job_has_queue.rb
+++ lib/rubocop/cop/root_cops/job_has_queue.rb
@@ -2,15 +2,16 @@ module RuboCop
   module Cop
     module RootCops
       class JobHasQueue < Cop
-        MESSAGE = "Configure the job to run in a specific queue using queue_as or use a whitelisted mixin.".freeze
+        MESSAGE = "Configure the job to run in a specific queue using queue_as, sharded_queue_as or a whitelisted mixin.".freeze
         MIXIN_WHITELIST = %i[LookupQueueConcern NewBusinessQuoteCreationQueueConcern RatesQueueConcern].freeze
+        QUEUEING_OPTIONS = [:queue_as, :sharded_queue_as].freeze
 
         def on_class(node)
           class_name = node.to_a[0].to_a[1]
           return unless class_name.to_s.end_with?("Job")
 
           send_descendants = node.descendants.select(&:send_type?)
-          unless send_descendants.any? { |d| d.to_a[1] == :queue_as || _whitelisted_mixin?(d) }
+          unless send_descendants.any? { |d| QUEUEING_OPTIONS.include?(d.to_a[1]) || _whitelisted_mixin?(d) }
             add_offense(node, :location => :expression, :message => MESSAGE)
           end
         end
