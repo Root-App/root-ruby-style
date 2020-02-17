c457a5cfa4e1639ef73f5f40f9bc497ad45926dd
John Andrews <john.andrews@joinroot.com>
2020-02-17 16:18:44 -0500

    Implements RootCops/MustInherit
    A configurable enforcer of the proper parentage for a whole directory.
    Initially made for engines/claims/app/jobs to enforce that we're inheriting
    properly from ClaimsJob. It wasn't too much extra effort to make it configurable
    for other scenarios, so I added that as well.

---
+++ lib/rubocop/cop/root_cops/must_inherit.rb
@@ -0,0 +1,57 @@
+module RuboCop
+  module Cop
+    module RootCops
+      # Classes in this directory must inherit from a specific class
+      #
+      # @example
+      #
+      # Given the following config:
+      # RootCops/MustInherit:
+      #   Mapping:
+      #     - Dir: engines/claims/app/jobs/
+      #       ParentClass: ClaimsJob
+      #
+      #   # bad
+      #   class BackfillClaimJob < ApplicationJob # ...
+      #
+      #   # good
+      #   class BackfillClaimJob < ClaimsJob # ...
+
+      class MustInherit < Cop
+        # entirely so i can stub this in the tests
+        def self.expand_path(filename)
+          File.expand_path(filename)
+        end
+
+        def investigate(processed_source)
+          @source_file_path = self.class.expand_path(processed_source.buffer.name)
+        end
+
+        def_node_matcher :find_class_inheritance, <<~PATTERN
+          (class
+           (const _ $_child)
+           {(const _ $_parent) $_noparent}
+           _)
+        PATTERN
+
+        def on_class(node)
+          return unless (config_superclass_name = parent_class_for_current_file)
+
+          find_class_inheritance(node) do |class_name, superclass_name|
+            if class_name.to_s != config_superclass_name && superclass_name.to_s != config_superclass_name
+              add_offense(node, :location => :expression, :message => "Classes in this directory must inherit from #{config_superclass_name}")
+            end
+          end
+        end
+
+        def mapping
+          cop_config["Mapping"] || []
+        end
+
+        def parent_class_for_current_file
+          mapping.detect { |m| @source_file_path.include?(m["Dir"]) }&.fetch("ParentClass")
+        end
+      end
+    end
+  end
+end
---
+++ spec/rubocop/cop/root_cops/must_inherit_spec.rb
@@ -0,0 +1,84 @@
+require_relative "../../../spec_helper.rb"
+
+RSpec.describe RuboCop::Cop::RootCops::MustInherit, :config do
+  subject(:cop) { described_class.new(config) }
+
+  let(:cop_config) do
+    {
+      "Mapping" => [
+        {
+          "Dir" => "app/jobs",
+          "ParentClass" => "MyJob"
+        }
+      ]
+    }
+  end
+
+  around do |example|
+    before = RuboCop::ConfigLoader.default_configuration[described_class.cop_name]
+    RuboCop::ConfigLoader.default_configuration[described_class.cop_name] = {}
+    example.run
+    RuboCop::ConfigLoader.default_configuration[described_class.cop_name] = before
+  end
+
+  context "when the class is inside a configured directory" do
+    before do
+      allow(described_class).to receive(:expand_path).and_return("/home/me/app/jobs/greetings_job.rb")
+    end
+
+    it "reports an offense when the class doesn't inherit from configured class" do
+      expect_offense(<<~RUBY.strip_indent)
+        class GreetingsJob < ApplicationJob
+        ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Classes in this directory must inherit from MyJob
+        end
+      RUBY
+    end
+
+    it "reports an offense when the class doesn't inherit from anything" do
+      expect_offense(<<~RUBY.strip_indent)
+        class GreetingsJob
+        ^^^^^^^^^^^^^^^^^^ Classes in this directory must inherit from MyJob
+        end
+      RUBY
+    end
+
+    it "reports no offenses when the class is the parent class definition" do
+      expect_no_offenses(<<~RUBY.strip_indent)
+        class MyJob < ApplicationJob
+        end
+      RUBY
+    end
+
+    it "reports no offense when specifies the proper class" do
+      expect_no_offenses(<<~RUBY.strip_indent)
+        class GreetingsJob < MyJob
+        end
+      RUBY
+    end
+
+    it "doesn't get messed up by class contents" do
+      expect_offense(<<~RUBY.strip_indent)
+        class GreetingsJob < ApplicationJob
+        ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Classes in this directory must inherit from MyJob
+          queue_as :super_high_priority
+
+          def perform(*args)
+          end
+        end
+      RUBY
+    end
+  end
+
+  context "when the class is not inside a configured directory" do
+    before do
+      allow(described_class).to receive(:expand_path).and_return("/home/me/app/controllers/greetings_controller.rb")
+    end
+
+    it "reports no offense" do
+      expect_no_offenses(<<~RUBY.strip_indent)
+        class GreetingsController < ApplicationController
+        end
+      RUBY
+    end
+  end
+end
