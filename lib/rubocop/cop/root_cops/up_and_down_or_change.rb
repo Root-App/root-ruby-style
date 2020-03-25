46b83c0cb217b4739b2149df64a5fb2906828157
Kyle Decot <kyle@joinroot.com>
2020-03-25 14:42:42 -0400

    Use Parent Class of Node

--- lib/rubocop/cop/root_cops/up_and_down_or_change.rb
+++ lib/rubocop/cop/root_cops/up_and_down_or_change.rb
@@ -5,9 +5,10 @@ module RuboCop
         MSG = "Migration must have either #change or #up/#down".freeze
 
         def on_class(node)
-          return unless active_record_migration?(node)
+          return unless active_record_migration?(node.parent_class)
 
           methods = node.each_descendant(:def).map { |n| n.children[0] }
+
 
           return if methods.include?(:change)
           return if methods.include?(:up) && methods.include?(:down)
