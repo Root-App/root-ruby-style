48338b66a59a6a63b3f5be24b813803213e645e0
Kyle Decot <kyle@joinroot.com>
2020-03-25 14:15:10 -0400

    Fix Issue Where Constant Is Already Defined

--- spec/rubocop/cop/root_cops/no_tracers_spec.rb
+++ spec/rubocop/cop/root_cops/no_tracers_spec.rb
@@ -1,12 +1,7 @@
 RSpec.describe RootCops::NoTracers do
-  OFFENDING_FORMS = [
-    "Tracer.trace_method".freeze,
-    "Tracer.any_method".freeze
-  ].freeze
-
   subject(:cop) { described_class.new }
 
-  OFFENDING_FORMS.each do |offending_form|
+  ["Tracer.trace_method", "Tracer.any_method"].each do |offending_form|
     it_behaves_like "registers an offense", offending_form, described_class::MSG
   end
 end
