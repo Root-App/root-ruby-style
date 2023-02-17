fc0a841dffb637429adcbe530c4c628a471f209d
joseph-hughes <joseph.hughes@joinroot.com>
2023-02-17 16:54:18 -0500

    Remove 'try' from UseLonelyOperator cop, since 'try' is actually useful

--- lib/rubocop/cop/root_cops/use_lonely_operator.rb
+++ lib/rubocop/cop/root_cops/use_lonely_operator.rb
@@ -2,11 +2,12 @@ module RuboCop
   module Cop
     module RootCops
       class UseLonelyOperator < Cop
-        MSG = "Use the lonely operator foo&.bar instead of foo.try(:bar)".freeze
+        MSG = "Use the lonely operator foo&.bar instead of foo.try!(:bar)".freeze
 
         def on_send(node)
           _receiver, method_name = *node
-          if %i[try try!].include?(method_name) && node.arguments?
+
+          if method_name == :try! && node.arguments?
             add_offense(node, :location => :expression, :message => MSG)
           end
         end
